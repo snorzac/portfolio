ca3d5c353983ef69fb08245e4581938f0e599c23
Isaac Tan <isaactannnnn@gmail.com>
2026-03-05 19:01:09 +0800

    sttuff

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 // typing animation
 var typed = new Typed(".auto-type",
     {
-        strings: ["Isaac Tan"],
+        strings: ["Hello! I am Isaac :)"],
         typeSpeed: 60,
         onComplete: function()
         {
             new Typed(".auto-type2",
                 {
-                    strings: ["cybersecurity and digital forensics graduate"],
+                    strings: ["i am cybersecurity and digital forensics graduate from temasek polytechnic."],
                     typeSpeed: 20,
-                    showCursor: false
+                    showCursor: false,
+                    onComplete: function()
+                    {
+                        new Typed(".auto-type3",
+                            {
+                                strings: ["i have a strong passion for programming and software development, mainly for school work and personal projects."],
+                                typeSpeed: 20,
+                                showCursor: false,
+                                onComplete: function()
+                                {
+                                    new Typed(".auto-type4",
+                                        {
+                                            strings: ["academics aside, i enjoy speedcubing, playing rhythm games, and watching shows during my free time to unwind."],
+                                            typeSpeed: 20,
+                                            showCursor: false
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
                 }
             )
         }
