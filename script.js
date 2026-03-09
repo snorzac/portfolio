1f0ba25b31be05b4cc45fbcc7021dba2426e2df4
Isaac Tan <isaactannnnn@gmail.com>
2026-03-09 23:18:43 +0800

    stuff

--- script.js
+++ script.js
@@ -2,36 +2,19 @@
 var typed = new Typed(".auto-type",
     {
         strings: ["Hello! I am Isaac :)"],
-        typeSpeed: 60,
-        onComplete: function()
-        {
-            new Typed(".auto-type2",
-                {
-                    strings: ["i am cybersecurity and digital forensics graduate from temasek polytechnic."],
-                    typeSpeed: 20,
-                    showCursor: false,
-                    onComplete: function()
-                    {
-                        new Typed(".auto-type3",
-                            {
-                                strings: ["i have a strong passion for programming and software development, mainly for school work and personal projects."],
-                                typeSpeed: 20,
-                                showCursor: false,
-                                onComplete: function()
-                                {
-                                    new Typed(".auto-type4",
-                                        {
-                                            strings: ["academics aside, i enjoy speedcubing, playing rhythm games, and watching shows during my free time to unwind."],
-                                            typeSpeed: 20,
-                                            showCursor: false
-                                        }
-                                    )
-                                }
-                            }
-                        )
-                    }
-                }
-            )
-        }
+        typeSpeed: 60
     }
 );
+
+// close navbar on mobile when nav link is clicked
+document.querySelectorAll('.nav-link').forEach(link =>
+{
+    link.addEventListener('click', () =>
+    {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        if (navbarCollapse.classList.contains('show'))
+        {
+            navbarCollapse.classList.remove('show');
+        }
+    });
+});
